24c95b2060d9e0d6001cb645a8bc66b586c43e0c
YanqunYang <2095291113@qq.com>
2024-05-08 01:05:14 +0800

    员工管理功能的开发

--- src/main/java/com/yanqun/service/EmpService.java
+++ src/main/java/com/yanqun/service/EmpService.java
@@ -1,5 +1,31 @@
 package com.yanqun.service;
 
+import com.yanqun.pojo.Emp;
+import com.yanqun.pojo.PageBean;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.util.List;
+
 //员工业务规则
 public interface EmpService {
+    /**
+     * 分页查询
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    PageBean page(Integer page, Integer pageSize,String name, Short gender,LocalDate begin,LocalDate end);
+
+    /**
+     * 批量删除
+     * @param ids
+     */
+    void delete(List<Integer> ids);
+
+    /**
+     * 新增员工
+     * @param emp
+     */
+    void save(Emp emp);
 }
